e88c4e702faf1b3d06e483570ed4a918b613e3b8
Abhishek Sachdev <sachdevabhi74@gmail.com>
2022-12-20 07:25:27 -0600

    Add_multiple_embedding_support

--- custom_embedding_dataset.py
+++ custom_embedding_dataset.py
@@ -6,6 +6,7 @@ from torchvision import transforms
 from torch.utils.data.dataset import Dataset
 import io
 import numpy as np
+import ast 
 
 class EmbeddingDataset(Dataset):
 	def __init__(self, csv_path:str, data_path:str, transform = None):
@@ -19,11 +20,24 @@ class EmbeddingDataset(Dataset):
 			index = index.tolist()
 
 		fetched_row = self.data_info.iloc[index]
+		
 		img = torch.tensor(np.load(self.data_path + "/" + fetched_row[1])[0])
 		target = fetched_row[2]
+		
+		alternate_embeddings = ast.literal_eval(fetched_row[3])
+		alternate_images = []
+		for _, file_name in alternate_embeddings.items():
+			alternate_images.append(torch.tensor(np.load(self.data_path + "/" + file_name)[0])) 
+		
 		if self.transform:
 			img = self.transform(img)
-		return img, target
+
+			alternate_images_t = []
+			for alt in alternate_images:
+				alternate_images_t.append(self.transform(alt))
+			alternate_images = alternate_images_t
+
+		return img, target, alternate_images
 
 
 	def __len__(self):
---
+++ stacked_model_fine_tuning.py
@@ -0,0 +1,169 @@
+from custom_embedding_dataset import EmbeddingDataset
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import os
+import argparse
+import networks.resnet as resnet
+import timm
+
+# makes things look nice
+from progress_bar import progress_bar
+
+
+def get_MLP(in_dim, out_dim):
+    return nn.Sequential(
+        nn.Linear(in_dim, 3072),
+        nn.GELU(),
+        nn.Dropout(),
+        nn.Linear(3072, 3072),
+        nn.GELU(),
+        nn.Dropout(),
+        nn.Linear(3072, 3072),
+        nn.GELU(),
+        nn.Dropout(),
+        nn.Linear(3072, 500),
+        nn.Linear(500, out_dim)
+    )
+
+
+def get_dataset(name, batch_size):
+    classes = ('adversarial', 'clean')
+    transform = None
+
+    if name == 'ciless_vit_mlp_embedding':
+        trainset = EmbeddingDataset("adv_datasets/ciless_embeddings_vit_low_with_mlp_embeddings_train/mapping.csv", "adv_datasets/ciless_embeddings_vit_low_with_mlp_embeddings_train", transform=transform)
+        testset = EmbeddingDataset("adv_datasets/ciless_embeddings_vit_low_with_mlp_embeddings_test/mapping.csv", "adv_datasets/ciless_embeddings_vit_low_with_mlp_embeddings_test", transform=transform)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2, drop_last=True)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2, drop_last=True)
+        classes = ('adversarial', 'clean')
+
+    elif name == 'ciless_vit_v2_mlp_embedding':
+        trainset = EmbeddingDataset("adv_datasets/ciless_embeddings_vit_with_mlp_embeddings_train/mapping.csv", "adv_datasets/ciless_embeddings_vit_with_mlp_embeddings_train", transform=transform)
+        testset = EmbeddingDataset("adv_datasets/ciless_embeddings_vit_with_mlp_embeddings_test/mapping.csv", "adv_datasets/ciless_embeddings_vit_with_mlp_embeddings_test", transform=transform)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2, drop_last=True)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2, drop_last=True)
+
+    else:
+        raise ValueError('Unknown dataset: {}'.format(name))
+    return trainloader, testloader, classes
+
+
+def get_model(model:str, num_classes:int, load=False):
+    if model == "mlp":
+        mlp = get_MLP(778, num_classes)
+        return mlp
+
+
+def train(epoch, max_epochs, net, trainloader, optimizer, scheduler, criterion, device, steps_per_update=1):
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    steps_per_update = 1
+    step = 0
+    for batch_idx, (inputs, targets, alternate_images) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        stacked_tensor = inputs
+        for img in alternate_images:    
+            stacked_tensor = torch.concat((stacked_tensor, img), 1)
+        
+        outputs = net(stacked_tensor)
+        
+        # scheduler.step()
+        loss = criterion(outputs, targets)
+        loss.backward()
+        step += 1
+        if step % steps_per_update == 0:
+            optimizer.step()
+            optimizer.zero_grad()
+            step = 0
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        progress_bar(epoch, max_epochs, batch_idx, len(trainloader), 'Loss: %.3f   Acc: %.3f%%'
+                     % (train_loss/(batch_idx+1), 100.*correct/total))
+    scheduler.step()
+
+
+def test(epoch, max_epochs, net, testloader, criterion, device):
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets, alternate_images) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+
+            stacked_tensor = inputs
+            for img in alternate_images:    
+                stacked_tensor = torch.concat((stacked_tensor, img), 1)
+
+            outputs = net(stacked_tensor)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            progress_bar(epoch, max_epochs, batch_idx, len(testloader), 'Loss: %.3f   Acc: %.3f%%'
+                         % (test_loss/(batch_idx+1), 100.*correct/total))
+    return float(correct)/total
+
+
+def fit_model(model, trainloader, testloader, device, epochs:int, learning_rate:float, sched_decay:float, step_size:int, save_path:str, steps_per_update:int):
+    best_acc = -1
+    best_name = ""
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, weight_decay=1e-4)
+    # optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size, sched_decay)
+    # scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, learning_rate, epochs=epochs, steps_per_epoch=len(trainloader))
+    # scaler = torch.cuda.amp.GradScaler(enabled=True)
+
+    for epoch in range(epochs):
+        train(epoch, epochs, model, trainloader, optimizer, scheduler, criterion, device, steps_per_update)
+        acc = test(epoch, epochs, model, testloader, criterion, device)
+        if acc > best_acc:
+            if best_name != "":
+                os.remove(best_name)
+            best_acc = acc
+            best_name = save_path + "_" + str(epoch) + ".pth"
+            torch.save(model.state_dict(), best_name)
+    f = open(save_path + "_best.txt", "w")
+    f.write(str(best_acc))
+    f.close()
+    return best_name, best_acc
+
+
+def main(dataset:str, model_name:str, epochs:int, learning_rate:float, batch_size:int, step_size:int, sched_decay:float, output_prefix:str, steps_per_update:int, load:bool):
+    print("CUDA Available: ", torch.cuda.is_available())
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    trainloader, testloader, classes = get_dataset(dataset, batch_size)    
+    model = get_model(model_name, len(classes), load)
+    model.to(device)
+    os.makedirs("trained_models/" + dataset + "/" + model_name +"/", exist_ok=True)
+    best_name, best_accuracy = fit_model(model, trainloader, testloader, device, epochs, learning_rate, sched_decay, step_size, "trained_models/" + dataset + "/" + model_name + "/" + output_prefix + dataset + "_" + model_name, steps_per_update)
+    print("Training complete: " + best_name + " with accuracy: " + str(round(best_accuracy, 4)))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Train a model on a dataset')
+    parser.add_argument('--dataset', type=str, default='ciless_vit_mlp_embedding', help='Dataset to train on')
+    parser.add_argument('--model', type=str, default='mlp', help='Model to train')
+    parser.add_argument('--output_prefix', type=str, default='', help='Prefix to add to model name, to avoid overlapping experiments.')
+    parser.add_argument('--epochs', type=int, default=150, help='Number of epochs to train')
+    parser.add_argument('--learning_rate', type=float, default=3e-5, help='Learning rate')
+    parser.add_argument('--batch_size', type=int, default=32, help='Batch size')
+    parser.add_argument('--steps_per_update', type=int, default=4, help='Number of steps per each epoch (For minibatching to save memory)')
+    parser.add_argument('--sched_decay', type=float, default=0.5, help='Scheduler Decay')
+    parser.add_argument('--step_size', type=int, default=50, help='Step Size For Scheduler')
+    parser.add_argument('--load', type=bool, default=True, help='Load best model')
+    args = parser.parse_args()
+    
+    main(args.dataset, args.model, args.epochs, args.learning_rate, args.batch_size, args.step_size, args.sched_decay, args.output_prefix, args.steps_per_update, args.load)
